12d40fc62ed2eb71039e2b328556edccfffb5139
Anton Kalinin <mrkalininanton@gmail.com>
2017-09-17 18:31:28 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Графы
+//
+//  Created by Антон Калинин on 17.09.17.
+//  Copyright © 2017 KalininAnton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
